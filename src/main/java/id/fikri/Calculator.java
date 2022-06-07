b3ac84058e4252ef8e81728f81b3d4accff11053
fikri <fuadfikri01@gmail.com>
2022-06-07 20:02:48 +0700

    test throw exception

--- src/main/java/id/fikri/Calculator.java
+++ src/main/java/id/fikri/Calculator.java
@@ -5,4 +5,12 @@ public class Calculator {
     public Integer add(Integer a, Integer b) {
         return a + b;
     }
+
+    public Integer divide(Integer a, Integer b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("cannot divide by zero");
+        } else {
+            return a / b;
+        }
+    }
 }
--- src/test/java/id/fikri/CalculatorTest.java
+++ src/test/java/id/fikri/CalculatorTest.java
@@ -1,6 +1,7 @@
 package id.fikri;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
@@ -13,5 +14,13 @@ public class CalculatorTest {
         var result = calculator.add(10,10);
         System.out.println(result);
         Assertions.assertEquals(20, result);
+    }
+
+    @DisplayName("test fail pembagian dengan 0")
+    @Test
+    void testDivideFail() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculator.divide(10,0);
+        });
     }
 }
