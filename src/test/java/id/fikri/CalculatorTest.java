17a84ce4faf7c29bb32dcc067572b5afd2315211
fikri <fuadfikri01@gmail.com>
2022-06-07 19:37:32 +0700

    test success

---
+++ src/test/java/id/fikri/CalculatorTest.java
@@ -0,0 +1,17 @@
+package id.fikri;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+
+    @Test
+    void testAddSuccess() {
+        var result = calculator.add(10,10);
+        System.out.println(result);
+        Assertions.assertEquals(20, result);
+    }
+}
